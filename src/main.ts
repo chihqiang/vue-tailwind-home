c85c13c195dd360e33e7e4a47282b6cf86088c5d
zhiqiang <zhiqiang2033@gmail.com>
2025-08-14 18:21:30 +0800

    🐛 fix: i8n

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { i18n } from './i18n'
+import { createI18n } from "vue-i18n";
+import en from "./i18n/en";
+import zh from "./i18n/zh";
 import './style.css'
 const app = createApp(App)
-app.use(i18n)
+
+app.use(createI18n({
+  legacy: false,
+  locale: "en", // 默认英文
+  messages: { en, zh },
+}))
+
+
 app.mount('#app')
